2ebe689eb2b036137dfad9188d81a9bfcfb58afc
AntonBerks <chief.keef2016@yandex.ru>
2026-09-06 14:31:49 +0300

    fix(5d): remove duplicate spatial presentation exports

--- client/packages/spatial/src/index.ts
+++ client/packages/spatial/src/index.ts
@@ -19,5 +19,5 @@ export * from './renderer';
 export * from './spatialInteraction';
 export * from './socialActions';
 export * from './spatialAffordances';
-export * from './geometry';
+export { type BerxSpatialPresentation, presentationForKind } from './geometry';
 export * from './mediaSurface';
